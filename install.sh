25321c7f68c2e121bc352a3b1e15079bd88dd213
abdellatif-dev <abdellatif.devlog@gmail.com>
2021-03-27 00:31:03 +0100

    cleaning script

--- install.sh
+++ install.sh
@@ -9,17 +9,17 @@ txtwht=$(tput setaf 7) # White
 txtrst=$(tput sgr0) # Text reset.
 
 echo ${txtgrn}installing grub theme-abdellatif ${txtrst}
-echo -e "${txtcyn}by\033[1m abdellatif-dev${txtrst}"
-echo -e "${txtblu}github =${txtgrn}\033[1m https://github.com/abdellatif-dev${txtrst}"
-echo -e "${txtblu}twitter =${txtgrn}\033[1mhttps://twitter.com/DevAbdellatif${txtrst}"
-echo -e "${txtblu}youtube =${txtgrn}\033[1mhttps://www.youtube.com/channel/UCsHub1wcuNlWtkGiVchRtzA${txtrst}"
+echo "${txtcyn}by\033[1m abdellatif-dev${txtrst}"
+echo "${txtblu}github =${txtgrn}\033[1m https://github.com/abdellatif-dev${txtrst}"
+echo "${txtblu}twitter =${txtgrn}\033[1mhttps://twitter.com/DevAbdellatif${txtrst}"
+echo "${txtblu}youtube =${txtgrn}\033[1mhttps://www.youtube.com/channel/UCsHub1wcuNlWtkGiVchRtzA${txtrst}"
 
 sleep 1
-echo  -e "${txtylw}\033[1mconfiguring grub ${txtrst}"
+echo "${txtylw}\033[1mconfiguring grub ${txtrst}"
 cp grub /etc/default/grub
 echo "cp grub /etc/default/grub"
-echo  -e "${txtylw}\033[1minstalling the theme${txtrst}"
-echo  -e "${txtpur}\033[1msudo cp grub-abdellatif/ /boot/grub/themes/ -r${txtrst}"
-sudo cp  grub-abdellatif/ /boot/grub/themes/ -rv
-echo  -e "${txtpur}\033[1msudo grub-mkconfig -o /boot/grub/grub.cfg${txtrst}"
+echo "${txtylw}\033[1minstalling the theme${txtrst}"
+echo "${txtpur}\033[1msudo cp grub-abdellatif/ /boot/grub/themes/ -r${txtrst}"
+sudo cp grub-abdellatif/ /boot/grub/themes/ -rv
+echo "${txtpur}\033[1msudo grub-mkconfig -o /boot/grub/grub.cfg${txtrst}"
 sudo grub-mkconfig -o /boot/grub/grub.cfg
